d993e359c98bf47a70b36bd882ed705f5684afee
JerloPH <jewelcarlosalvador@gmail.com>
2020-09-05 11:49:28 +0800

    FIX: Clicking on Cancel closes the form.

--- HomeCinema/frmSettings.Designer.cs
+++ HomeCinema/frmSettings.Designer.cs
@@ -267,6 +267,7 @@
             this.btnCancel.TabIndex = 11;
             this.btnCancel.Text = "CANCEL";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnSave
             // 
