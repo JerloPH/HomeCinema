4087cdf7e35b777adea18c9df7b399f3b0f42484
JerloPH <jewelcarlosalvador@gmail.com>
2020-09-22 08:53:01 +0800

    GUI: RES: Changed Loading form looks and Icon.

--- HomeCinema/frmLoading.Designer.cs
+++ HomeCinema/frmLoading.Designer.cs
@@ -31,42 +31,18 @@ namespace HomeCinema
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLoading));
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.picBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(0, 16);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(482, 65);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "PLEASE WAIT";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(2, 360);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(482, 57);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "WHILE LOADING";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // picBox
             // 
             this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.picBox.InitialImage = null;
-            this.picBox.Location = new System.Drawing.Point(105, 88);
+            this.picBox.Location = new System.Drawing.Point(12, 12);
             this.picBox.Name = "picBox";
-            this.picBox.Size = new System.Drawing.Size(256, 256);
+            this.picBox.Size = new System.Drawing.Size(182, 263);
             this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBox.TabIndex = 2;
             this.picBox.TabStop = false;
@@ -75,11 +51,9 @@ namespace HomeCinema
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(482, 444);
+            this.ClientSize = new System.Drawing.Size(206, 287);
             this.ControlBox = false;
             this.Controls.Add(this.picBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -96,8 +70,6 @@ namespace HomeCinema
         }
 
         #endregion
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox picBox;
     }
 }
