b9c431ffb2c82319abe7a7dfd7dafdbe9a14abfc
JerloPH <jewelcarlosalvador@gmail.com>
2020-12-28 19:59:55 +0800

    Updated version for release.

--- HomeCinema/Properties/AssemblyInfo.cs
+++ HomeCinema/Properties/AssemblyInfo.cs
@@ -33,6 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.8.0")]
-[assembly: AssemblyFileVersion("0.0.8.0")]
-[assembly: NeutralResourcesLanguage("en-PH")]
+[assembly: AssemblyVersion("0.0.9.0")]
+[assembly: AssemblyFileVersion("0.0.9.0")]
