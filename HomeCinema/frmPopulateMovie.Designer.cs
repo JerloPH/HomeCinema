161f820a81b23074f1557ea71666a3c66c795328
JerloPH <jewelcarlosalvador@gmail.com>
2021-02-20 17:27:02 +0800

    REV: frmPopulateMovie.cs : Edited

--- HomeCinema/frmPopulateMovie.Designer.cs
+++ HomeCinema/frmPopulateMovie.Designer.cs
@@ -29,12 +29,80 @@ namespace HomeCinema
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPopulateMovie));
+            this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.escapeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BackgroundWorker
+            // 
+            this.BackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INSERT MESSAGE HERE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(134, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 34);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // escapeButton
+            // 
+            this.escapeButton.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.escapeButton.Location = new System.Drawing.Point(77, 90);
+            this.escapeButton.Name = "escapeButton";
+            this.escapeButton.Size = new System.Drawing.Size(145, 38);
+            this.escapeButton.TabIndex = 126;
+            this.escapeButton.TabStop = false;
+            this.escapeButton.Text = "Cancel (ESC)";
+            this.escapeButton.UseVisualStyleBackColor = false;
+            this.escapeButton.Visible = false;
+            this.escapeButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // frmPopulateMovie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(293, 89);
+            this.ControlBox = false;
+            this.Controls.Add(this.escapeButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPopulateMovie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "main";
             this.Text = "frmPopulateMovie";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPopulateMovie_FormClosing);
+            this.Shown += new System.EventHandler(this.frmPopulateMovie_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmPopulateMovie_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        public System.ComponentModel.BackgroundWorker BackgroundWorker;
+        public System.Windows.Forms.Button escapeButton;
     }
 }
--- HomeCinema/frmPopulateMovie.cs
+++ HomeCinema/frmPopulateMovie.cs
@@ -12,9 +12,71 @@ namespace HomeCinema
 {
     public partial class frmPopulateMovie : Form
     {
-        public frmPopulateMovie()
+        public string Caption
+        {
+            get { return this.Text; }
+            set { this.Text = value; }
+        }
+        public string Message
+        {
+            get { return label1.Text; }
+            set
+            {
+                if (label1.InvokeRequired)
+                {
+                    BeginInvoke((Action)delegate
+                    {
+                        label1.Text = value;
+                    });
+                }
+                else
+                    label1.Text = value;
+            }
+        }
+        public bool isCanceled { get; set; }
+        public int TopPosition { get; set; }
+        public frmPopulateMovie(string message, string caption)
         {
             InitializeComponent();
+            Message = message;
+            Caption = caption;
+            isCanceled = false;
+            TopPosition = 0;
+        }
+
+        private void frmPopulateMovie_Shown(object sender, EventArgs e)
+        {
+            if (BackgroundWorker.IsBusy)
+                return;
+            BackgroundWorker.RunWorkerAsync();
+            if (TopPosition != 0)
+                this.Top = TopPosition;
+        }
+
+        private void frmPopulateMovie_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (BackgroundWorker.IsBusy)
+                e.Cancel = true;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            isCanceled = true;
+        }
+
+        private void frmPopulateMovie_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    escapeButton.PerformClick();
+                    break;
+            }
         }
     }
 }
