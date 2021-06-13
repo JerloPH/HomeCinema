471007bfabd9652f2eabd55614023788924f6698
JerloPH <jewelcarlosalvador@gmail.com>
2021-06-13 11:28:36 +0800

    GUI: Improved 'About' form

--- HomeCinema/frmAbout.Designer.cs
+++ HomeCinema/frmAbout.Designer.cs
@@ -29,20 +29,43 @@ namespace HomeCinema
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAbout));
             this.txtLicense = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtLicense
             // 
             this.txtLicense.BackColor = System.Drawing.Color.Black;
+            this.txtLicense.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtLicense.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtLicense.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.txtLicense.Location = new System.Drawing.Point(12, 12);
+            this.txtLicense.Location = new System.Drawing.Point(12, 104);
             this.txtLicense.Multiline = true;
             this.txtLicense.Name = "txtLicense";
             this.txtLicense.ReadOnly = true;
-            this.txtLicense.Size = new System.Drawing.Size(769, 471);
-            this.txtLicense.TabIndex = 0;
+            this.txtLicense.ShortcutsEnabled = false;
+            this.txtLicense.Size = new System.Drawing.Size(769, 379);
+            this.txtLicense.TabIndex = 1;
+            this.txtLicense.TabStop = false;
+            this.txtLicense.Text = resources.GetString("txtLicense.Text");
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.Color.Black;
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtTitle.Location = new System.Drawing.Point(12, 12);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.ReadOnly = true;
+            this.txtTitle.ShortcutsEnabled = false;
+            this.txtTitle.Size = new System.Drawing.Size(769, 86);
+            this.txtTitle.TabIndex = 2;
+            this.txtTitle.TabStop = false;
+            this.txtTitle.Text = "HomeCinema - Organize your Movie Collection\r\n© 2020-2021  JerloPH (https://github" +
+    ".com/JerloPH)";
             // 
             // frmAbout
             // 
@@ -50,16 +73,17 @@ namespace HomeCinema
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(793, 495);
+            this.Controls.Add(this.txtTitle);
             this.Controls.Add(this.txtLicense);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAbout";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About HomeCinema";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(frmAbout_FormClosing);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmAbout_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,5 +92,6 @@ namespace HomeCinema
         #endregion
 
         private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.TextBox txtTitle;
     }
 }
--- HomeCinema/frmAbout.cs
+++ HomeCinema/frmAbout.cs
@@ -17,24 +17,16 @@ namespace HomeCinema
             InitializeComponent();
             // Form properties
             Icon = GlobalVars.HOMECINEMA_ICON;
-            txtLicense.Text = @"
-    HomeCinema - Organize your Movie Collection
-    Copyright (C) 2020  JerloPH (https://github.com/JerloPH)
+            BackColor = GlobalVars.SET_COLOR_BG;
+            ForeColor = GlobalVars.SET_COLOR_FONT;
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+            txtTitle.BackColor = GlobalVars.SET_COLOR_BG;
+            txtLicense.BackColor = GlobalVars.SET_COLOR_BG;
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+            txtTitle.ForeColor = GlobalVars.SET_COLOR_FONT;
+            txtLicense.ForeColor = GlobalVars.SET_COLOR_FONT;
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-            ";
-            //txtLicense.BackColor = System.Drawing.SystemColors.Window;
+            Focus();
         }
         // ######################################################### Methods
         // ######################################################### Events
@@ -42,6 +34,7 @@ namespace HomeCinema
         {
             // Closed
             GlobalVars.formAbout = null;
+            Dispose();
         }
     }
 }
