90b79e05d090f1875931222e4c20bce804ffbb4f
JerloPH <jewelcarlosalvador@gmail.com>
2021-07-11 12:47:56 +0800

    Add MediaLocations structure

---
+++ HomeCinema/Structs/MediaLocations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema
+{
+    public struct MediaLocations
+    {
+        public MediaLocations(string path, string mediatype, string src)
+        {
+            Path = path;
+            MediaType = mediatype;
+            Source = src;
+        }
+
+        public string Path { get; }
+        public string MediaType { get; }
+        public string Source { get; }
+    }
+}
