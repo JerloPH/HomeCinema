e7ce60770290ef109c54f88e1b4574f15389e323
JerloPH <jewelcarlosalvador@gmail.com>
2021-07-24 15:21:04 +0800

    NEW: Anilist JSON object and Wrapper

---
+++ HomeCinema/Entity/AnilistAPI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HomeCinema.Entity
+{
+    public static class AnilistAPI
+    {
+        private static string Host = "https://graphql.anilist.co";
+        
+        private static string BuildSearchPaginated(string text)
+        {
+            string search = text.Replace("\"", string.Empty);
+            string query = @"query {
+              Page (page: 1, perPage: 20)
+              {
+                pageInfo {
+                  total
+                  currentPage
+                  lastPage
+                  hasNextPage
+                  perPage
+                }
+                media (type: ANIME, search: " + search + @") {
+                  id
+                  title {
+                    romaji
+                  }
+                  coverImage {
+                    medium
+                  }
+                }
+              }
+            }";
+            return query;
+        }
+    }
+}
---
+++ HomeCinema/JSONs/AnilistPageQuery.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace HomeCinema
+{
+    public class AnilistPageQuery
+    {
+        [JsonProperty("data")]
+        public AnilistDataPaginated Data { get; set; }
+    }
+    public class AnilistDataPaginated
+    {
+        [JsonProperty("Page")]
+        public AnilistPage Page { get; set; }
+    }
+    public class AnilistPage
+    {
+        [JsonProperty("pageInfo")]
+        public AnilistPageInfo PageInfo { get; set; }
+        [JsonProperty("media")]
+        public List<AnilistMediaPageResult> MediaList { get; set; }
+    }
+    public class AnilistPageInfo
+    {
+        [JsonProperty("total")]
+        public int Total { get; set; } = 0;
+        [JsonProperty("currentPage")]
+        public int CurrentPage { get; set; } = 0;
+        [JsonProperty("lastPage")]
+        public int LastPage { get; set; } = 0;
+        [JsonProperty("perPage")]
+        public int PerPage { get; set; } = 0;
+        [JsonProperty("hasNextPage")]
+        public bool HasNextPage { get; set; } = false;
+    }
+    public class AnilistMediaPageResult
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; } = 0;
+        [JsonProperty("title")]
+        public AnilistMediaTitle Title { get; set; }
+        [JsonProperty("coverImage")]
+        public AnilistMediaCoverImage CoverImage { get; set; }
+    }
+    public class AnilistMediaTitle
+    {
+        [JsonProperty("romaji")]
+        public string Romaji { get; set; } = "";
+    }
+    public class AnilistMediaCoverImage
+    {
+        [JsonProperty("medium")]
+        public string Medium { get; set; } = "";
+    }
+}
