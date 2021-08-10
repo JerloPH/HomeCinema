66430541a9aa88efd7180bee5d23d7c979d7c1c5
JerloPH <jewelcarlosalvador@gmail.com>
2021-08-10 21:30:15 +0800

    FIX: Anilist API not fetching english titles

--- HomeCinema/Entity Static/AnilistAPI.cs
+++ HomeCinema/Entity Static/AnilistAPI.cs
@@ -76,6 +76,7 @@ namespace HomeCinema
                 }
     
                   title {
+                    english
                     romaji
                   }
                   startDate {
